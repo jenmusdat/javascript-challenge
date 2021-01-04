11b741310aca8fb3a39529b702341a70daba9d89
Robert and Jenny <jenny_nickles@hotmail.com>
2021-01-03 22:52:54 -0800

    worked on part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,36 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Use D3 to select the table body
+var tbody = d3.select("tbody");
+
+
+// Append one table row `tr` to the table body
+var row = tbody.append("tr");
+
+//loop through the cells to add all the data
+data.forEach(function(data))=>{
+
+// Append one cell for the student name
+var cell = row.append("td")
+cell.text(ufo.datetime)
+
+cell = row.append("td")
+cell.text(ufo.city)
+
+cell = row.append("td")
+cell.text(ufo.state)
+
+cell = row.append("td")
+cell.text(ufo.country)
+
+cell = row.append("td")
+cell.text(ufo.shape)
+
+cell = row.append("td")
+cell.text(ufo.durationMinutes)
+
+cell = row.append("td")
+cell.text(ufo.comments)
+
+}
