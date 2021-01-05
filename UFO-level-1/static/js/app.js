3630e6c21cf666fd3caef8b4dda20465386be70e
Robert and Jenny <jenny_nickles@hotmail.com>
2021-01-05 07:11:21 -0800

    fixed part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,59 +5,43 @@ var ufoData = data;
 var tbody = d3.select("tbody");
 
 // Select the button
-var button = d3.select("#button");
-
-// Select the form
-var form = d3.select("#form");
+var button = d3.select("#filter-btn");
 
 // Create event handlers 
 button.on("click", runEnter);
-form.on("submit", runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
-
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  
+ tbody.html("")
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("enter date");
+  //duplicate next three lines and instead of var filterData use filterData = filterData.filter
+  var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
+  var filterData = data.filter(obj => obj.datetime == inputValue)
+  console.log(filterData);
+  displayufodata(filterData);
   console.log(inputValue);
 }
-// Append one table row `tr` to the table body
-var row = tbody.append("tr");
+
 
 //add a function here
 function displayufodata(ufoData){
-}
 //loop through the cells to add all the data
-data.forEach(function(data) => { 
+ufoData.forEach((line) => { 
 
-// Append one cell for the student name
+// Append one table row `tr` to the table body
+var row = tbody.append("tr");
+
+Object.values(line).forEach((values)=>{
 var cell = row.append("td")
-cell.text(ufo.datetime)
-
-cell = row.append("td")
-cell.text(ufo.city)
-
-cell = row.append("td")
-cell.text(ufo.state)
-
-cell = row.append("td")
-cell.text(ufo.country)
-
-cell = row.append("td")
-cell.text(ufo.shape)
-
-cell = row.append("td")
-cell.text(ufo.durationMinutes)
-
-cell = row.append("td")
-cell.text(ufo.comments)
+cell.text(values)
 
 })
+
+
 })
+}
+displayufodata(data)
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,36 +1,49 @@
 // from data.js
-var tableData = data;
+var ufoData = data;
 
 // Use D3 to select the table body
 var tbody = d3.select("tbody");
 
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Create event handlers 
+button.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+ tbody.html("")
+  // Select the input element and get the raw HTML node
+  //duplicate next three lines and instead of var filterData use filterData = filterData.filter
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  var filterData = data.filter(obj => obj.datetime == inputValue)
+  console.log(filterData);
+  displayufodata(filterData);
+  console.log(inputValue);
+}
+
+
+//add a function here
+function displayufodata(ufoData){
+//loop through the cells to add all the data
+ufoData.forEach((line) => { 
 
 // Append one table row `tr` to the table body
 var row = tbody.append("tr");
 
-//loop through the cells to add all the data
-data.forEach(function(data))=>{
-
-// Append one cell for the student name
+Object.values(line).forEach((values)=>{
 var cell = row.append("td")
-cell.text(ufo.datetime)
+cell.text(values)
 
-cell = row.append("td")
-cell.text(ufo.city)
+})
 
-cell = row.append("td")
-cell.text(ufo.state)
 
-cell = row.append("td")
-cell.text(ufo.country)
-
-cell = row.append("td")
-cell.text(ufo.shape)
-
-cell = row.append("td")
-cell.text(ufo.durationMinutes)
-
-cell = row.append("td")
-cell.text(ufo.comments)
+})
+}
+displayufodata(data)
 }
 
